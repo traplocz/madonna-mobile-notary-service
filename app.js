4eec2ca551941b79df88a55703a34057108f1d16
Julian Antoine <JulianKingAntoine@Gmail.com>
2021-08-20 21:50:39 -0400

    updating app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type' : 'text/html' })
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('TrapLocz said file has not been found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log('TrapLocz said something went wrong', error)
+    } else {
+        console.log('TrapLocz said server is listening on port' + port)
+    }
+})
